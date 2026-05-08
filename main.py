7846c26eca315688ff1d32da2ed5e748a52093d8
Ikwuka CySec <242934426+IkwukaCySec@users.noreply.github.com>
2026-05-08 13:03:00 -0400

    Complete project

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 # Building a Report Card Printer
 # Steps taken will be recorded as follows, below.
 
-'''
+
 # Step 1:
 name = 'Alice'
 
 # Step 2
 print(name) # Outputs: Alice
-'''
 
 # Step 6
 print(name, type(name)) # Outputs: Alice <class 'str'>
@@ -20,3 +19,20 @@ is_student = True
 
 # Step 5
 print(is_student, type(is_student)) # Outputs: True <class 'bool'>
+
+# Step 6
+print(name, type(name)) # Outputs: Alice <class 'str'>
+
+# Step 7
+age = 20
+print(age, type(age)) # Outputs: 20 <class 'int'>
+
+# Step 8
+score = 80.5
+print(isinstance(score, int)) # Outputs: False
+
+# Step 9
+print(isinstance(score, float)) # Outputs: True
+
+# Step 10
+print(score, type(score)) # Outputs: 80.5 <class 'float'>
