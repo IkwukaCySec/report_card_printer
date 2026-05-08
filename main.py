16ad0dfce0f0e6b8a924f3c151b42ad496005ccd
Ikwuka CySec <242934426+IkwukaCySec@users.noreply.github.com>
2026-05-08 12:52:55 -0400

    Commnt out multiple lines of code and proffer solution to Step 6 challenge

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 # Building a Report Card Printer
 # Steps taken will be recorded as follows, below.
 
+'''
 # Step 1:
 name = 'Alice'
 
 # Step 2
 print(name) # Outputs: Alice
+'''
+
+# Step 6
+print(name, type(name)) # Outputs: Alice <class 'str'>
 
 # Step 3
 print(type(name)) # Outputs: <class 'str'>
