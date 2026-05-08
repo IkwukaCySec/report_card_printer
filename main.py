80b7adfdc04c4c0a9712ed59e24d8a0ef55a6ccc
Ikwuka CySec <242934426+IkwukaCySec@users.noreply.github.com>
2026-05-08 12:36:17 -0400

    Add main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+# Building a Report Card Printer
+# Steps taken will be recorded as follows, below.
+
+# Step 1:
+# Create a variable called name and assign the word 'Alice' as its value.
+name = 'Alice'
+print(name)
