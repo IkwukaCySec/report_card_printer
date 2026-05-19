ff54f337bc387db943be3f162ddef4a8255b3852
Ikwuka Okoye <ikwukao@proton.me>
2026-05-19 12:13:27 -0400

    Format code properly

--- main.py
+++ main.py
@@ -3,36 +3,36 @@
 
 
 # Step 1:
-name = 'Alice'
+name = "Alice"
 
 # Step 2
-print(name) # Outputs: Alice
+print(name)  # Outputs: Alice
 
 # Step 6
-print(name, type(name)) # Outputs: Alice <class 'str'>
+print(name, type(name))  # Outputs: Alice <class 'str'>
 
 # Step 3
-print(type(name)) # Outputs: <class 'str'>
+print(type(name))  # Outputs: <class 'str'>
 
 # Step 4
 is_student = True
 
 # Step 5
-print(is_student, type(is_student)) # Outputs: True <class 'bool'>
+print(is_student, type(is_student))  # Outputs: True <class 'bool'>
 
 # Step 6
-print(name, type(name)) # Outputs: Alice <class 'str'>
+print(name, type(name))  # Outputs: Alice <class 'str'>
 
 # Step 7
 age = 20
-print(age, type(age)) # Outputs: 20 <class 'int'>
+print(age, type(age))  # Outputs: 20 <class 'int'>
 
 # Step 8
 score = 80.5
-print(isinstance(score, int)) # Outputs: False
+print(isinstance(score, int))  # Outputs: False
 
 # Step 9
-print(isinstance(score, float)) # Outputs: True
+print(isinstance(score, float))  # Outputs: True
 
 # Step 10
-print(score, type(score)) # Outputs: 80.5 <class 'float'>
+print(score, type(score))  # Outputs: 80.5 <class 'float'>
